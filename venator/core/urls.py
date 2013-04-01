1156edecc48399f14c350c666ade57caecf857e1
Guilherme Vierno <guilhermevierno@gmail.com>
2013-04-01 20:47:32 -0300

    Initial front-end development.

--- venator/core/urls.py
+++ venator/core/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import patterns, include, url
 from venator.core.views import Homepage
 
 
-urlpatterns = patterns('venator.core.views',
+urlpatterns = patterns(
+    'venator.core.views',
     url(r'^$', Homepage.as_view(), name='homepage'),
+    url(r'^(?P<page>\w+)/$', 'pages', name='pages')
 )
--- venator/core/views.py
+++ venator/core/views.py
@@ -1,6 +1,12 @@
 # Create your views here.
 
 from django.views.generic import TemplateView
+from django.shortcuts import render_to_response
+
 
 class Homepage(TemplateView):
     template_name = "index.html"
+
+
+def pages(request, page):
+    return render_to_response(page + ".html")
