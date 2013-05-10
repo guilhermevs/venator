0432626b979ceb934720f9d628274dc327043009
Guilherme Vierno <guilhermevierno@gmail.com>
2013-05-09 21:56:12 -0300

    Attachment on bug

--- venator/fasttracker/models.py
+++ venator/fasttracker/models.py
@@ -14,6 +14,9 @@ class Bug(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     creator = models.CharField(max_length=30)
     status = models.CharField(max_length=2, choices=STATUS, default="PE")
+    attachment = models.FileField(upload_to="fasttracker",
+                                  null=True,
+                                  blank=True)
 
     def get_absolute_url(self):
         return r("fasttracker:bugdetail", args=(self.id,))
--- venator/urls.py
+++ venator/urls.py
@@ -1,4 +1,7 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -10,4 +13,4 @@ urlpatterns = patterns(
     url(r'^fasttracker/',
         include('venator.fasttracker.urls', namespace='fasttracker')),
     url(r'', include('venator.core.urls', namespace='core')),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
